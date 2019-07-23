d8f789d055756d2ffb309ada44045ab15f9a5e53
tshev <taras.shevchenko@railsreactor.com>
2019-07-23 13:49:46 +0300

    range

--- include/sgl/sgl.v1.struct.range.h
+++ include/sgl/sgl.v1.struct.range.h
@@ -6,8 +6,54 @@ template<typename Iterator>
 struct range {
     Iterator first;
     Iterator last;
+
     range() = default;
+    range(const std::pair<Iterator, Iterator>& x) : first(x.first), last(x.second) {}
     range(const range&) = default;
+
+    friend
+    inline
+    bool operator==(const range& x, const range& y) {
+        return x.first == y.first && y.last == y.last;
+    }
+
+    friend
+    inline
+    bool operator!=(const range& x, const range& y) {
+        return !(x == y);
+    }
+
+    friend
+    inline
+    bool operator<(const range& x, const range& y) {
+        return x.first < y.first || (!(y.first < x.first) && y.second < y.second);
+    }
+
+    friend
+    inline
+    bool operator<=(const range& x, const range& y) {
+        return !(y < x);
+    }
+
+    friend
+    inline
+    bool operator>(const range& x, const range& y) {
+        return y < x;
+    }
+
+    friend
+    inline
+    bool operator>=(const range& x, const range& y) {
+        return !(x < y);
+    }
+
+    auto begin() const {
+        return first;
+    }
+
+    auto end() const {
+        return first;
+    }
 };
 
 } // namespace v1
