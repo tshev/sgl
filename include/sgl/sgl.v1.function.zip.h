a97b5b779991782c483725b914815ad9b4e04b2b
tshev <taras@tshev.com>
2019-07-25 18:26:56 +0300

    type

--- include/sgl/sgl.v1.function.zip.h
+++ include/sgl/sgl.v1.function.zip.h
@@ -39,7 +39,7 @@ template<typename It0, typename It1, typename F>
 inline
 std::pair<It0, It1> zip(It0 f0, It0 l0, It1 f1, It1 l1, F f) {
     typedef typename std::iterator_category<It0>::iterator_category iterator_category0;
-    typedef typename std::iterator_category<It1>::iterator_category iterator_category0;
+    typedef typename std::iterator_category<It1>::iterator_category iterator_category1;
 
     constexpr bool enable_forward_iterator =
         std::is_same<iterator_category0, std::random_access_iterator_tag>::value &&
