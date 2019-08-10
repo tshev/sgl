d0ba11a015bc2187462ef0b8e386951e9d275a89
tshev <taras.shevchenko@railsreactor.com>
2019-08-10 23:53:29 +0300

    Add namespace sgl::v1

--- include/sgl/sgl.v1.function.find_by_mask.h
+++ include/sgl/sgl.v1.function.find_by_mask.h
@@ -4,15 +4,14 @@ namespace sgl {
 namespace v1 {
 
 template<typename It0, typename It1>
-// requires(ForwardIterator(It0) && ForwardIterator(It1) && Readable(It0) && Readable(It1))
 inline
-std::pair<It0, It1> find_by_mask(It0 first0, It0 last0, It1 first1) {
-    while (first0 != last0 && !*first1) {
-        ++first0;
+std::pair<It0, It1> find_by_mask(It0 first, It0 last, It1 first1) {
+    while (first != last && !*first1) {
+        ++first;
         ++first1;
     }
-    return {first0, first1};
+    return {first, first1};
 }
 
-}
-}
+} // namespace v1
+} // namespace sgl
