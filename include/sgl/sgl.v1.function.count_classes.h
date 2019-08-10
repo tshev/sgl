7e4f31112a4b51db52109a03ca9a4df4915f1eae
tshev <taras.shevchenko@railsreactor.com>
2019-08-10 18:28:29 +0300

    vim

---
+++ include/sgl/sgl.v1.function.count_classes.h
@@ -0,0 +1,13 @@
+#pragma once
+namespace sgl {
+namespace v1 {
+
+template<typename ForwardIterator>
+inline
+size_t count_classes(ForwardIterator first, ForwardIterator last) {
+    if (first != last) { return 0ul; }
+    return *std::max_element(first, last) + 1ul;
+}
+
+} // namespace v1
+} // namespace sgl
