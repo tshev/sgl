ada5739c49a0a0229faa0288e74cf42c6ec62058
tshev <taras.shevchenko@railsreactor.com>
2019-09-06 23:57:37 +0300

    Add memcpy for unaligned load

--- include/sgl/sgl.v1.function.copy.h
+++ include/sgl/sgl.v1.function.copy.h
@@ -40,13 +40,39 @@ void* copy(sgl::v1::simd_tag<true>, void const* first0, void* last0, void* out) 
     return out1;
 }
 
+
+void* copy(sgl::v1::simd_tag<false>, void const* first0, void* last0, void* out)  {
+    char const* first1 = (char const*)first0;
+    char const* last1 = (char const*)last0;
+    char* out1 = (char*)out;
+    size_t n0 = last1 - first1;
+    constexpr const size_t step = 32ul;
+    size_t n_head = (size_t)first1 % step;
+    if (n0 < n_head) {
+        while (first1 != last1) {
+            *out1 = *first1;
+            ++out1;
+            ++first1;
+        }
+        return out1;
+    }
+    char const* middle = first1 + n_head; 
+    while (first1 != middle) {
+        *out1 = *first1;
+        ++out1;
+        ++first1;
+    }
+    return sgl::v1::copy(sgl::v1::simd_tag<true>(), middle, last0, out1);
+}
+
+
 template<typename It, typename O>
 inline
 requires(ForwardIterator(It) && OutputIterator(O))
 O copy(It first, It last, O out) {
     typedef typename std::iterator_traits<It>::value_type T;
     if constexpr (sgl::v1::is_pointer<It>() && sgl::v1::is_pointer<O>() && sgl::v1::is_pod<T>()) {
-        return (O)sgl::v1::copy(sgl::v1::simd_tag<true>(), first, last, out);
+        return (O)sgl::v1::copy(sgl::v1::simd_tag<false>(), first, last, out);
     } else {
         while (first != last) {
             *out = *first;
