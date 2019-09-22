99f1b1869806c4dbac05351c0c5d9fc9f1ce4d01
tshev <taras.shevchenko@railsreactor.com>
2019-09-22 14:39:15 +0200

    fix contains all

--- include/sgl/sgl.v1.function.contains_all.h
+++ include/sgl/sgl.v1.function.contains_all.h
@@ -10,7 +10,7 @@ inline
 bool contains_all(ForwardIterator0 f0, ForwardIterator0 l0, ForwardIterator1 f1, ForwardIterator1 l1) {
     while (f1 != l1) {
         auto pos = sgl::v1::lower_bound(f0, l0, *f1);
-        if (== l0 || *pos != *f1) { return false; }
+        if (pos == l0 || *f1 < *pos) { return false; }
         ++f1;
     }
     return true;
@@ -34,7 +34,7 @@ inline
 bool contains_all(ForwardIterator0 f0, ForwardIterator0 l0, BinaryPredicate binary_pred, Projection, ForwardIterator1 f1, ForwardIterator1 l1) {
     while (f1 != l1) {
         auto pos = sgl::v1::lower_bound(f0, l0, *f1, binary_pred);
-        if (poss == l0 || binary_pred(*f1, *pos)) { return false; }
+        if (pos == l0 || binary_pred(*f1, *pos)) { return false; }
         ++f1;
     }
     return true;
@@ -47,7 +47,7 @@ inline
 ForwardIterator1 inclusion(ForwardIterator0 f0, ForwardIterator0 l0, BinaryPredicate binary_pred, Projection projection, ForwardIterator1 f1, ForwardIterator1 l1) {
     while (f1 != l1) {
         auto pos = sgl::v1::lower_bound(f0, l0, *f1, binary_pred, projection);
-        if (poss == l0 || binary_pred(*f1, projection(*pos))) { return false; }
+        if (pos == l0 || binary_pred(*f1, projection(*pos))) { return false; }
         ++f1;
     }
     return true;
