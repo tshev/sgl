cf435d5214daf7d36a7b9127a70b6be909495cb1
Taras Shevchenko <cgeek95@gmail.com>
2020-05-24 13:44:30 +0300

    Implement fast log2 for built-in integer

--- include/sgl/sgl.v1.function.log2.h
+++ include/sgl/sgl.v1.function.log2.h
@@ -2,19 +2,20 @@
 namespace sgl {
 namespace v1 {
 
-template<typename T>
+template <typename T>
 inline
 T log2(T x) {
-    if (std::is_same<T, unsigned char>::value || std::is_same<T, char>::value) {
+    // assert(0 < x);
+    if constexpr (std::is_same<T, unsigned char>::value || std::is_same<T, char>::value) {
         return log2<int>(x);
-    } else if (std::is_same<T, unsigned short>::value || std::is_same<T, short>::value) {
+    } else if constexpr (std::is_same<T, unsigned short>::value || std::is_same<T, short>::value) {
         return log2<int>(x);
     } else if constexpr (std::is_same<T, unsigned int>::value || std::is_same<T, int>::value) {
-        return T(sizeof(T)) * T(8)  - T(1) - __builtin_clz(x);
-    } else if constexpr (std::is_same<T, unsigned long>::value) {
-        return T(sizeof(T)) * T(8)  - T(1) - __builtin_clzl(x);
-    } else if constexpr (std::is_same<T, unsigned long long>::value) {
-        return T(sizeof(T)) * T(8)  - T(1) - __builtin_clzll(x);
+        return T(sizeof(T)) * T(8) - T(1) - __builtin_clz(x);
+    } else if constexpr (std::is_same<T, unsigned long>::value || std::is_same<T, long>::value) {
+        return T(sizeof(T)) * T(8) - T(1) - __builtin_clzl(x);
+    } else if constexpr (std::is_same<T, unsigned long long>::value || std::is_same<T, long long>::value) {
+        return T(sizeof(T)) * T(8) - T(1) - __builtin_clzll(x);
     } else {
         return std::log2(x);
     }
