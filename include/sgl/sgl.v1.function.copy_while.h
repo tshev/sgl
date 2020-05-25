8108b4b4669774dde8806133746146cce1714a94
Taras Shevchenko <cgeek95@gmail.com>
2020-05-25 18:47:34 +0300

    Extend copy_while algorithm

--- include/sgl/sgl.v1.function.copy_while.h
+++ include/sgl/sgl.v1.function.copy_while.h
@@ -2,14 +2,39 @@
 
 namespace sgl {
 namespace v1 {
-template<typename ForwardIterator, typename OutputIterator, typename Predicate>
-std::pair<ForwardIterator, OutputIterator> copy_while(ForwardIterator first, OutputIterator output, Predicate predicate) {
-  while (predicate(*first)) {
+
+template<typename It0, typename It1, typename P>
+requires(
+    sgl::v1::input_iterator(It0) && sgl::v1::readable(It0) &&
+    sgl::v1::forward_iterator(It1) && sgl::v1::writable(It1) &&
+    sgl::v1::binary_predicate<SGLValueType(It0)>(P)
+)
+inline
+std::pair<It0, It1> copy_while(It0 first, It1 output, P pred) {
+  while (pred(*first)) {
     *output = *first;
     ++output;
     ++first;
   }
   return {first, output};
 }
+
+
+template<typename It0, typename It1, typename P>
+requires(
+    sgl::v1::input_iterator(It0) && sgl::v1::readable(It0) &&
+    sgl::v1::forward_iterator(It1) && sgl::v1::writable(It1) &&
+    sgl::v1::binary_predicate<SGLValueType(It0)>(P)
+)
+inline
+std::pair<It0, It1> copy_while(It0 first, It0 last, It1 output, P pred) {
+  while (first != last && pred(*first)) {
+    *output = *first;
+    ++output;
+    ++first;
+  }
+  return {first, output};
+}
+
 } // namespace v1
 } // namespace sgl
