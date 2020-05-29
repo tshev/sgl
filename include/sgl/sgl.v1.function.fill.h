d325bf34805a28e39234e7de0ee98cc1841c300f
Taras Shevchenko <cgeek95@gmail.com>
2020-05-29 17:14:27 +0300

    Fix stupid bugs

--- include/sgl/sgl.v1.function.fill.h
+++ include/sgl/sgl.v1.function.fill.h
@@ -26,7 +26,7 @@ template<typename V, typename T>
 requires(sgl::v1::builtin_type(T))
 void fill(sgl::v1::simd_tag<false, V>, T* first, T* last, const T& value) noexcept {
     constexpr const size_t block_size = sizeof(V);
-    constexpr const size_t tail_size = sgl::v1::max(4ul, sizeof(T));
+    constexpr const size_t tail_size = sgl::v1::max(block_size, sizeof(T));
     constexpr const size_t buffer_size = block_size + tail_size;
     constexpr const size_t blocks_count = 8ul;
     constexpr const size_t step = block_size * blocks_count;
@@ -34,10 +34,10 @@ void fill(sgl::v1::simd_tag<false, V>, T* first, T* last, const T& value) noexce
     static_assert(buffer_size % sizeof(value) == 0ul);
 
     char buffer[buffer_size];
-
+    
     sgl::v1::fill(sgl::v1::default_tag{}, (T*)&buffer[0], (T*)&buffer[buffer_size], value);
     sgl::v1::fill(sgl::v1::default_tag{}, first, first + sgl::v1::min<size_t>(last - first, buffer_size / sizeof(T)), value);
-	const size_t padding = sgl::v1::alignment_padding<size_t>(size_t(first), 32ul);
+	const size_t padding = sgl::v1::alignment_padding<size_t>(size_t(first), block_size);
 
     V* first1 = (V*)((char*)first + padding);
     V* last1 = (V*)((char*)first1 + ((char*)last - (char*)first1) / step * step);
