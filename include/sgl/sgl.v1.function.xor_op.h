0e02c0ee52057b70a62988086090f7a1d19f58eb
Taras Shevchenko <cgeek95@gmail.com>
2020-05-30 15:51:41 +0300

    Improve the implementation of SIMD vectors

--- include/sgl/sgl.v1.function.xor_op.h
+++ include/sgl/sgl.v1.function.xor_op.h
@@ -8,17 +8,18 @@ __m128i xor_op(const __m128i x, const __m128i y) noexcept {
     return _mm_xor_si128(x, y);
 }
 
+#ifdef __AVX2__
 inline
 __m256i xor_op(const __m256i x, const __m256i y) noexcept {
     return _mm256_xor_si256(x, y);
 }
+#endif
 
 template<typename T>
 inline
 T xor_op(const T x, const T y) noexcept {
     return xor_op(x.value, y.value);
 }
-
 
 } // namespace v1
 } // namespace sgl
