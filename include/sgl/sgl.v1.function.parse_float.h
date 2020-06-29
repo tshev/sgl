c59a558286560fae752ff7aead4413438ab8e95f
Taras Shevchenko <cgeek95@gmail.com>
2020-06-29 15:27:04 +0300

    Fix float parser

--- include/sgl/sgl.v1.function.parse_float.h
+++ include/sgl/sgl.v1.function.parse_float.h
@@ -25,28 +25,28 @@ It parse_float(It first, It last, ForwardIterator out) {
 
     D exp = 0;
     if (exp_first != last && *exp_first == '+') { ++exp_first; }
-    auto last_exp = sgl::v1::parse_int(exp_first, last, &exp);
+    It last_exp = sgl::v1::parse_int(exp_first, last, &exp);
     if (last_exp != last) {
         return last_exp;
     }
 
 
     T x0 = 0;
-    auto p0 = sgl::v1::parse_int(first_integer, last_integer, &x0);
+    It p0 = sgl::v1::parse_int(first_integer, last_integer, &x0);
     if (p0 != last_integer) return p0;
 
     T x1 = 0;
-    auto p1 = sgl::v1::parse_int(first_frac, last_frac, &x1);
+    It p1 = sgl::v1::parse_int(first_frac, last_frac, &x1);
     if (p1 != last_frac) return p1;
    
-
     if (exp < 0) {
         x0 = x0 / sgl::v1::pow(T(10), -exp);
         x1 = x1 / sgl::v1::pow(T(10), -exp + sgl::v1::distance(first_frac, last_frac));
-    } else {
+    } else  if (0 < exp) {
         x0 = x0 * sgl::v1::pow(T(10), exp);
         x1 = x1 * sgl::v1::pow(T(10), exp - sgl::v1::distance(first_frac, last_frac));
     }
+
 
     if (x0 < x1) {
         *out = x0;
@@ -55,6 +55,7 @@ It parse_float(It first, It last, ForwardIterator out) {
         *out = x1;
         *out += x0;
     }
+
 
     return last_exp;
 }
