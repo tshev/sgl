ee78bf2bf0256323732730b959ad5defd8c15fe9
tshev <cgeek95@gmail.com>
2020-08-23 02:47:21 +0300

    Use simd_vector

--- include/sgl/sgl.v1.function.delta_encode.h
+++ include/sgl/sgl.v1.function.delta_encode.h
@@ -31,20 +31,19 @@ void delta_encode(It first, It last, T value) {
 template<typename V, typename T>
 inline
 void delta_encode(sgl::v1::simd_tag<false, V>, T* first, T* last) {
-    typedef typename sgl::v1::simd_vector<T, sgl::v1::max_simd_vector_size<T>::value * 8 * sizeof(T)> vector_type;
-    constexpr const size_t size = sgl::v1::max_simd_vector_size<T>::value;
-    if (last - first <= 2 * size) {
+    constexpr const size_t N = sgl::v1::max_simd_vector_size<T>::value * 8 * sizeof(T);
+    typedef typename sgl::v1::simd_vector<T, N> vector_type;
+    if (last - first <= 2 * vector_type::size) {
         sgl::v1::delta_encode(first, last);
         return;
     }
-/*
+
     sgl::v1::simd_vector_iterator<T, N> first1{first};
     vector_type prev_value = *first1;
     ++first1;
-    size_t offset = ((last - first) / size) * size - size;
-    first += offset;
-    sgl::v1::simd_vector_iterator<T, N> last1{first1 + offset};
     constexpr const size_t step = vector_type::size - 1;
+    first += (last - first + 1) / vector_type::size * vector_type::size - step;
+    sgl::v1::simd_vector_iterator<T, N> last1{first};
 
     while (first1 != last1) {
         vector_type delta = *first1 - prev_value;
@@ -52,15 +51,16 @@ void delta_encode(sgl::v1::simd_tag<false, V>, T* first, T* last) {
         first1.sink() = delta; 
         first1 += vector_type::size;
     }
-    T buffer[size];
-    prev_value(buffer);
+    T buffer[vector_type::size];
+    sgl::v1::simd_vector_iterator<T, N>{buffer}.sink() = prev_value;
     sgl::v1::delta_encode(first, last, buffer[0]);
-*/
-
+    return;
  
+    /*
     vector_type prev_value = vector_type(first);
     ++first;
     T* last1 = first + ((last - first) / size) * size - size;
+    std::cout << "D = " << last1 - first << std::endl;
     while (first != last1) {
         vector_type delta = vector_type(first) - prev_value;
         prev_value = vector_type(first + vector_type::size - 1);
@@ -69,7 +69,9 @@ void delta_encode(sgl::v1::simd_tag<false, V>, T* first, T* last) {
     }
     T buffer[size];
     prev_value(buffer);
+    std::cout << "B = " << buffer[0] << std::endl;
     sgl::v1::delta_encode(first, last, buffer[0]);
+    */
 }
 
 /*
