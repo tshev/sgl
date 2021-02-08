b5fa4e02fe40449742ee953ac0bfe625e7d10338
tshev <cgeek95@gmail.com>
2021-02-08 17:28:34 +0200

    And URI component encoder

---
+++ include/sgl/sgl.v1.function.encode_uri_component.h
@@ -0,0 +1,28 @@
+#pragma one
+
+namespace sgl {
+namespace v1 {
+
+template<typename It, typename Out>
+Out encode_uri_component(It first, It last, Out out) {
+    const char *hex = "0123456789ABCDEF";
+    while (first != last) {
+        if ((*first >= 'a' && *first <= 'z') || (*first >= 'A' && *first <= 'Z') || (*first >= '0' && *first <= '9')||  *first == '-' || *first == '_' || *first == '.' || *first == '~') {
+            *out = *first;
+            ++out;
+        } else {
+            *out = '%';
+            ++out;
+            auto c = static_cast<unsigned char>(*first);
+            *out = hex[c >> 4];
+            ++out;
+            *out = hex[c & 15];
+            ++out;
+        }
+        ++first;
+    }
+    return out;
+}
+
+}
+}
---
+++ include/sgl/sgl.v1.function.encoded_uri_component_length.h
@@ -0,0 +1,26 @@
+#pragma one
+
+namespace sgl {
+namespace v1 {
+
+template<typename It, typename N>
+N encoded_uri_component_length(It first, It last, N n) {
+    while (first != last) {
+        if ((*first >= 'a' && *first <= 'z') || (*first >= 'A' && *first <= 'Z') || (*first >= '0' && *first <= '9')||  *first == '-' || *first == '_' || *first == '.' || *first == '~') {
+            ++n;
+        } else {
+            n += 3;
+        }
+        ++first;
+    }
+    return n;
+}
+
+template<typename It>
+size_t encoded_uri_component_length(It first, It last) {
+    return sgl::v1::encoded_uri_component_length(first, last, size_t{0});
+}
+
+
+}
+}
