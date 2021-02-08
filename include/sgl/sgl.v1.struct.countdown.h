89071139d6dbb55d77fb5f9b8b9b2fb35df08fa4
tshev <cgeek95@gmail.com>
2021-02-08 17:31:47 +0200

    Implement a countdown

---
+++ include/sgl/sgl.v1.struct.countdown.h
@@ -0,0 +1,39 @@
+#pragma onace
+namespace sgl {
+namespace v1 {
+template<typename N>
+struct countdown : sgl::v1::totally_ordered<sgl::v1::countdown<N>> {
+    N value; 
+
+    countdown() : value(0) {}
+    countdown(N n) : value(n) {}
+    countdown(const countdown&) = default;
+
+    friend
+    inline
+    bool operator==(const countdown& x, const countdown& y) {
+        return x == y;
+    }
+
+    friend
+    inline
+    bool operator!=(const countdown& x, const countdown& y) {
+        return !(x == y);
+    }
+
+    friend
+    inline
+    bool operator<(const countdown& x, const countdown& y) {
+        return x.value < y.value;
+    }
+
+    bool operator()() {
+        if (value == 0) {
+            return false;
+        }
+        --value;
+        return true;
+    }
+};
+} // namespace v1
+} // namespace sgl
